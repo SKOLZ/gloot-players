76b13bd5337ee7f649b1763ef2fcbd9a1b870263
SKOLZ <gabriel.zanzotti@gmail.com>
2022-09-14 14:50:56 +0200

    Added styles for the player edition feature.

--- src/screens/Players/components/PlayerRow/index.tsx
+++ src/screens/Players/components/PlayerRow/index.tsx
@@ -7,6 +7,8 @@ import { REQUIRED_ERROR_MSG, UNKNOWN_ERROR_MSG } from "../../../../constants/str
 import useToggle from "../../../../hooks/useToggle";
 import { ReactComponent as EditIcon } from '../../../../assets/edit-icon.svg';
 import { ReactComponent as DeleteIcon } from '../../../../assets/delete-icon.svg';
+import { ReactComponent as ConfirmIcon } from '../../../../assets/confirm-icon.svg';
+import { ReactComponent as CancelIcon } from '../../../../assets/cancel-icon.svg';
 import styles from './styles.module.scss';
 
 type PlayerRowProps = {
@@ -42,13 +44,19 @@ function PlayerRow({player, onDelete, onPlayerEdited}: PlayerRowProps) {
     <li key={player.id} className={styles.playerInfo}>
       {
         isEditing ? (
-          <form onSubmit={handleSubmit(handleEditPlayerSubmit)}>
+          <form className={styles.editForm} onSubmit={handleSubmit(handleEditPlayerSubmit)}>
             <div className="form-group">
-              <input {...register("name", { required: REQUIRED_ERROR_MSG })} id="name"/>
+              <input className={styles.editInput} {...register("name", { required: REQUIRED_ERROR_MSG })} id="name"/>
               { errors.name && <p className="contact-error">{errors.name.message}</p> }
             </div>
-            <button type="submit">apply</button>
-            <button type="button" onClick={cancelEdit}>cancel</button>
+            <span className={styles.playerActions}>
+              <button className="button" type="submit">
+                <ConfirmIcon className={`icon-tertiary ${styles.confirmIcon}`} />
+              </button>
+              <button className="button" type="button" onClick={cancelEdit}>
+                <CancelIcon className={`icon-tertiary ${styles.cancelIcon}`} />
+              </button>
+            </span>
           </form>
         ) : (
           <>
